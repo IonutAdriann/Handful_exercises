96b669729e6749f7f72c5d07967a0e3095a20e9d
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 21:29:57 +0100

    separate namespace

---
+++ SeparateNamespace/Calculator.cs
@@ -0,0 +1,23 @@
+﻿namespace SeparateNamespace;
+
+public class Calculator
+{
+    public int Add(int x, int y)
+    {
+        int sum = 0;
+
+        return sum = x + y;
+    }
+
+    public int Add(int[] integers)
+    {
+        var result = 0;
+
+        foreach (var n in integers)
+        {
+            result += n;
+        }
+
+        return result;
+    }
+}
---
+++ SeparateNamespace/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using SeparateNamespace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var calculator = new Calculator();
+        Console.WriteLine(calculator.Add(3, 4));
+        Console.WriteLine(calculator.Add(new int[] {1,2,3,4,5,6}));
+    }
+
+}
