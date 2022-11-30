b7940e5131928405a5829feb55647e3b34a862fb
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 20:09:50 +0100

    simple person example

---
+++ SimplePersonExample/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace SimplePersonExample;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public void Introduce()
+    {
+        System.Console.WriteLine($"Hi, my name is {Name}");
+    }
+}
---
+++ SimplePersonExample/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using SimplePersonExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Person();
+        person.Name = "Ionut";
+        person.Introduce();
+    }
+}
