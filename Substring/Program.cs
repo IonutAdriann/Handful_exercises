292c0bc6af3d0a7ce847a7a5f87c83c049248109
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 21:35:50 +0100

    substring

---
+++ Substring/Program.cs
@@ -0,0 +1,9 @@
+﻿
+string nTwice(string value, int n)
+{
+    return $"{value[..n]}{value.Substring(value.Length - n, n)}";
+}
+
+Console.WriteLine(nTwice("Hello", 2));
+Console.WriteLine(nTwice("Chocolate", 3));
+Console.WriteLine(nTwice("Chocolate", 1));
