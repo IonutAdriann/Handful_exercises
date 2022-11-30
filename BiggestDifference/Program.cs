f771e06a2fd05a21f8646e48ff8348be3e3c0486
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 21:36:58 +0100

    biggest difference

---
+++ BiggestDifference/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int BigDiff(int[] values)
+{
+    return values.Max() - values.Min();
+}
+
+Console.WriteLine(BigDiff(new[] {10, 3, 5, 6}));
+Console.WriteLine(BigDiff(new[] {7, 2, 10, 9}));
+Console.WriteLine(BigDiff(new[] {2, 10, 7, 2}));
