0f40561c2b2942a1a4514ed3389c49b56bf16b3d
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 21:33:42 +0100

    surround with

---
+++ SurroundWith/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+string MakeOutWord(string outer, string word)
+{
+    return $"{outer[..2]}{word}{outer.Substring(2, 2)}";
+}
+Console.WriteLine(MakeOutWord("<<>>", "Yay"));
+Console.WriteLine(MakeOutWord("<<>>", "WooHoo"));
+Console.WriteLine(MakeOutWord("[[]]", "Word"));
