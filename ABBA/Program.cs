66cf450065b7ac444bf6171cd6db3acb052d970b
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 21:32:36 +0100

    Abba

---
+++ ABBA/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Diagnostics.Metrics;
+
+string makeABBA(string a, string b)
+{
+    return $"{a}{b}{b}{a}";
+}
+
+Console.WriteLine(makeABBA("Hi", "Bye"));
+Console.WriteLine(makeABBA("Yo", "Alice"));
+Console.WriteLine(makeABBA("What", "Up"));
