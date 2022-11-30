08a800401c08eab7b111a8eb7900567b1eff4337
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 21:31:13 +0100

    more math ex 6

---
+++ MoreMath_ex6/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using static System.Int32;
+
+namespace MoreMath_ex6;
+
+public class Calculator
+{
+    public void Maximum()
+    {
+        Console.WriteLine("Input number: ");
+        TryParse(Console.ReadLine(), out var x);
+        
+        Console.WriteLine("Input number: ");
+        TryParse(Console.ReadLine(), out var y);
+        
+        Console.WriteLine($"The largest number is: {Math.Max(x, y)}");
+    }
+}
---
+++ MoreMath_ex6/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using MoreMath_ex6;
+
+var calculator = new Calculator();
+calculator.Maximum();
