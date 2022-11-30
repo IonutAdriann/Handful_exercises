485c29bcb36d84469666553ea53e07dbcb79112a
Ionut Adrian <ionutrotaru077@gmail.com>
2022-11-30 21:28:42 +0100

    printing numbers

---
+++ PrintingNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿PrintEvenNumbers(20);
+PrintUnevenNumbers(20);
+PrintDivisibleNumbers(20, 4);
+
+void PrintEvenNumbers(int x)
+{
+    Console.WriteLine($"Printing even numbers up to {x}:");
+    for (var i = 0; i <= x; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
+void PrintUnevenNumbers(int x)
+{
+    Console.WriteLine($"Printing uneven numbers up to {x}:");
+    for (var i = 0; i <= x; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
+void PrintDivisibleNumbers(int x, int y)
+{
+    Console.WriteLine($"Printing numbers divisible by {y} up to {x}:");
+    var n = 0;
+    while (n <= x)
+    {
+        if (n % y == 0)
+        {
+            Console.WriteLine(n);
+        }
+        n++;
+    }
+}
